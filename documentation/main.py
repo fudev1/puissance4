64b6414911a9ac2a266514e1c99de7b65d80a6bc
Matteo D.V. <matteo.divita@outlook.be>
2024-07-30 15:57:14 +0200

    doc: adding documentation for Tkinter + init app window

---
+++ documentation/main.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+
+"""
+# Créer la fenêtre principale de l'application
+# ➡️ Tk() = Fonction pour créer une fenêtre vide
+# ➡️ mainloop() est une méthode qui permet de lancer la boucle de répétition de l'application
+"""
+
+fenetre = Tk()
+fenetre.mainloop()
